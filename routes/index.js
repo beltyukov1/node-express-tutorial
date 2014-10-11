6072d56492e060dc32004171bb7794ba3c169a88
Alex Beltyukov <beltyukov.1@gmail.com>
2014-10-11 14:22:29 -0500

    Add ability to add user

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,30 @@ router.get('/userlist', function(req, res) {
   });
 });
 
+/* GET New User page. */
+router.get('/newuser', function(req, res) {
+  res.render('newuser', { title: 'Add New User' });
+});
+
+/* POST to Add User Service */
+router.post('/adduser', function(req, res) {
+  var db = req.db;
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+  var collection = db.get('usercollection');
+
+  collection.insert({
+    "username": userName,
+    "email": userEmail
+  }, function(err, doc) {
+    if (err) {
+      res.send("There was a problem adding information to database");
+    } else {
+      res.location("userlist");
+      res.redirect("userlist");
+    }
+  })
+
+});
+
 module.exports = router;
